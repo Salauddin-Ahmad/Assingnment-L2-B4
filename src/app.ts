930ebe11220a1967269bfcaaa63a56e953f47cfe
Salauddin-Ahmad <salauddinahmed555@gmail.com>
2025-03-19 12:00:00 +0000

    Refactor app.ts and server.ts

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Hello World!'
+  })
 })
 
 app.listen(port, () => {
